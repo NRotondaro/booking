bbf5fb1e50a9101dad80999d5c0a79553913bb6a
NRotondaro <niksonrotondaro1@gmail.com>
2023-05-03 10:07:22 -0300

    chore: restart project

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import authRoute from './routes/auth.js';
-import usersRoute from './routes/users.js';
-import hotelsRoute from './routes/hotels.js';
-import roomsRoute from './routes/rooms.js';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -24,28 +19,8 @@ mongoose.connection.on('disconnected', () => {
   console.log('mongoDB is disconnected!');
 });
 
-mongoose.connection.on('connected', () => {
-  console.log('mongoDB is connected!');
-});
-
-//middlewares
-app.use(cookieParser());
-app.use(express.json());
-
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/hotels', hotelsRoute);
-app.use('/api/rooms', roomsRoute);
-
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong!';
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
+app.get('/', (req, res) => {
+  res.send('hello first request');
 });
 
 app.listen(8800, () => {
