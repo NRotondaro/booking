9f43cc23a8bba05aef2d5e85d831e9987ea35f5e
NRotondaro <niksonrotondaro1@gmail.com>
2023-05-04 07:24:30 -0300

    feat: add login & jwt token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,27 @@
 import express from 'express';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/user.js';
 
 const router = express.Router();
+
+// CREATE
+router.post('/', createUser);
+
+// UPDATE
+router.put('/:id', updateUser);
+
+// DELETE
+router.delete('/:id', deleteUser);
+
+// GET
+router.get('/:id', getUser);
+
+// GET ALL
+router.get('/', getUsers);
 
 export default router;
